44ca86aa4bbf1fee622ee975eb0ff3ea80846af0
gabriel-aguiar-gafit <gabriel.aguiar@gafit.com.br>
2025-06-13 00:28:57 -0300

    feat: alteração no DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             StatusSeeder::class,
-            VeiculosSeeder::class,
+            VehiclesSeeder::class,
         ]);
     }
 }
