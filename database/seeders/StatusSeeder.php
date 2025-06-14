be3f5b63379e34b80edfaa3be6a24c3e858c7a33
Vinícius <vinicius.oli7dev@gmail.com>
2025-06-14 20:01:23 -0300

    refactor: alteração status disponivel no StatusSeeder.php

--- database/seeders/StatusSeeder.php
+++ database/seeders/StatusSeeder.php
@@ -14,7 +14,7 @@ class StatusSeeder extends Seeder
     public function run(): void
     {
         DB::table('status')->insert([
-            ['nome' => 'disponível', 'created_at' => now(), 'updated_at' => now()],
+            ['nome' => 'disponivel', 'created_at' => now(), 'updated_at' => now()],
             ['nome' => 'vendido', 'created_at' => now(), 'updated_at' => now()],
             ['nome' => 'indisponivel', 'created_at' => now(), 'updated_at' => now()],
             ['nome' => 'reservado', 'created_at' => now(), 'updated_at' => now()],
