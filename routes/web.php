459568bd6cedaac7efb3a29ff26b35afefb77930
gabriel-aguiar-gafit <gabriel.aguiar@gafit.com.br>
2025-06-18 00:14:52 -0300

    fix: ajuste da rota de deletar veículo de delete para get.

--- routes/web.php
+++ routes/web.php
@@ -9,9 +9,9 @@ Route::middleware(['web'])->group(function () {
     });
 
     Route::get('/veiculos', [VehicleController::class, 'index'])->name('veiculos.list');
-    Route::get('/veiculos/cadastrar', [VehicleController::class, 'create'])->name('veiculos.add');
+    Route::get('/veiculos/cadastrar', [VehicleController::class, 'createVehicle'])->name('veiculos.add');
     Route::post('/veiculos/salvar', [VehicleController::class, 'store'])->name('veiculos.store');
-    Route::delete('/veiculos/{id}/excluir', [VehicleController::class, 'delete'])->name('veiculos.delete');
+    Route::get('/veiculos/{id}/excluir', [VehicleController::class, 'delete'])->name('veiculos.delete');
 
 });
 
