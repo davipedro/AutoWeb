af9263bf2c15fe4c46337266a96ada19ffae8b7e
gabriel-aguiar-gafit <gabriel.aguiar@gafit.com.br>
2025-06-18 00:07:26 -0300

    feat: criando traduções para o front de status, transmissoes e combustiveis

---
+++ resources/lang/pt_BR/status.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'disponivel' => 'Dispónível',
+    'vendido' => 'Vendido',
+    'indisponivel' => 'Indisponível',
+    'reservado' => 'Reservado',
+    'manutencao' => 'Manutenção',
+    'inativo' => 'Inativo',
+];
