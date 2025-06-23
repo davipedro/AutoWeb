a1fd9cfa62f168c04e3d163c3fb7a55260486504
Vinícius <vinicius.oli7dev@gmail.com>
2025-06-23 18:06:00 -0300

    feat: Adicionando rotas CRUD clientes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CatalogController;
+use App\Http\Controllers\ClientController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\ProfileController;
@@ -15,6 +16,7 @@ Route::middleware(['web'])->group(function () {
         Route::get('/dashboard', [DashboardController::class, 'adminDashboard'])->name('admin.dashboard');
 
         Route::prefix('/veiculos')->group(function () {
+            Route::get('/veiculos', [VehicleController::class, 'index'])->name('veiculos.list');
             Route::get('/cadastrar', [VehicleController::class, 'createVehicle'])->name('veiculos.add');
             Route::post('/salvar', [VehicleController::class, 'store'])->name('veiculos.store');
             Route::get('/editar/{id}', [VehicleController::class, 'editVehicle'])->name('veiculos.edit');
@@ -22,16 +24,16 @@ Route::middleware(['web'])->group(function () {
             Route::get('/excluir/{id}', [VehicleController::class, 'delete'])->name('veiculos.delete');
         });
 
-        Route::post('register', [RegisteredUserController::class, 'store']);
-        Route::get('/register', [RegisteredUserController::class, 'create'])
-            ->name('register');
     });
+
+    Route::post('register', [RegisteredUserController::class, 'store']);
+    Route::get('/register', [RegisteredUserController::class, 'create'])
+        ->name('register');
 
     Route::prefix('seller')->middleware(['auth', 'seller'])->group(function () {
         Route::get('/dashboard', [DashboardController::class, 'sellerDashboard'])->name('seller.dashboard');
     });
 
-    Route::get('/veiculos', [VehicleController::class, 'index'])->name('veiculos.list');
     Route::get('/catalogo', [CatalogController::class, 'index'])->name('catalogo');
 
     Route::middleware('auth')->group(function () {
@@ -39,6 +41,16 @@ Route::middleware(['web'])->group(function () {
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
+
+    Route::group((array)'clientes', function () {(
+    Route::get('/clientes', [ClientController::class, 'index'])->name('clientes.list'));
+        Route::get('/cadastrar', [ClientController::class, 'createClient'])->name('clientes.add');
+        Route::post('/salvar', [ClientController::class, 'store'])->name('clientes.store');
+        Route::get('/editar/{id}', [ClientController::class, 'editClient'])->name('clientes.edit');
+        Route::post('/atualizar/{id}', [ClientController::class, 'update'])->name('clientes.update');
+        Route::get('/excluir/{id}', [ClientController::class, 'delete'])->name('clientes.delete');
+    });
+
 });
 
 require __DIR__.'/auth.php';
