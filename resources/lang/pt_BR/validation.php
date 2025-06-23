07043b2a58a85cc1a6747fee91c11c6ec5aa2b57
Vinícius <vinicius.oli7dev@gmail.com>
2025-06-23 18:05:41 -0300

    feat: Adicionando mensagem de erro para os clientes

--- resources/lang/pt_BR/validation.php
+++ resources/lang/pt_BR/validation.php
@@ -11,9 +11,10 @@ return [
         'string' => 'O campo :attribute não pode ter mais que :max caracteres.',
         'numeric' => 'O campo :attribute não pode ser maior que :max.',
     ],
-    'unique' => 'O campo :attribute informado já está em uso por outro veículo.',
+    'unique' => 'O valor :attribute informado já está em uso.',
 
     'attributes' => [
+        //Veiculos
         'marca' => 'marca',
         'modelo' => 'modelo',
         'ano' => 'ano',
@@ -25,6 +26,20 @@ return [
         'placa' => 'placa',
         'chassi' => 'chassi',
         'status' => 'status',
+
+        //Clientes
+        'nome_completo' => 'nome do cliente',
+        'data_nascimento' => 'data de nascimento',
+        'email' => 'e-mail',
+        'telefone' => 'telefone',
+        'cpf' => 'CPF',
+        'rg' => 'RG',
+        'endereco' => 'endereço',
+        'complemento' => 'complemento',
+        'cidade' => 'cidade',
+        'estado' => 'estado',
+        'cep' => 'CEP',
+
         'observacoes' => 'observações',
     ],
 ];
